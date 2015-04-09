0b1dbe58c68e7e0bfc7072b3dd42a84b9eb60ef9
Andrew Wood <aew61@case.edu>
2015-04-09 02:57:33 -0400

    provide abstract query representation
    Now we don't have to hardcode each query! We just have to supply an
    arguments list.

---
+++ src/crave/db/QueryTemplate.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class QueryTemplate extends Object {
+  
+  private HashMap<String, String> args_;
+  private Queue<String> query_;
+  
+  public QueryTemplate(String fileName) throws FileNotFoundException, IOException, Exception
+  {
+    super();
+    this.args_ = new HashMap<String, String>();
+    this.query_ = new LinkedList<String>();
+    
+    String p = "[a-zA-Z0-9]*";
+    Pattern pattern = Pattern.compile("(<" + p + ">" + p + "</" + p +">)+");
+    BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    Matcher m = null;
+    boolean matched = true;
+    String arg = null;
+    for(String line = reader.readLine(); line != null; line = reader.readLine())
+    {
+//      System.out.println("line: " + line + " length: " + line.length());
+      matched = true;
+      while(matched)
+      {
+        m = pattern.matcher(line);
+        if(m.find())
+        {
+          arg = m.group().trim().replaceAll("<" + p + ">", "").replaceAll("</" + p + ">", "");
+          if(arg == null || arg.equals(""))
+          {
+            throw new Exception("UNPREDICTED SYMBOL IN INPUT");
+          }
+          this.args_.put(arg, "*null*");
+          this.query_.add(line.substring(0, m.start()));
+          this.query_.add(arg);
+//          System.out.println("\tFOUND " + arg + " IN LINE AT START: " + m.start() + " END: " + m.end());
+          if(line.length() >= m.end() + 1) { line = line.substring(m.end(), line.length()); }
+          else
+          { 
+            m.replaceFirst(arg);
+            matched = false;
+          }
+//          System.out.println("\tNEW LINE: " + line);
+        }
+        else
+        { 
+          matched = false;
+          this.query_.add(line);
+        }
+      }
+    }
+  }
+  
+  public Set<String> getArgs() { return this.args_.keySet(); }
+  
+  public HashMap<String, String> getMap() { return this.args_; }
+  
+  public Queue<String> getQuery() { return this.query_; }
+
+  public Query makeQuery(Queue<QueryToken> tokens)
+  {
+//    System.out.println("ARG MAP: " + this.getMap() + "\nquery: " + this.getQuery() + "\ntokens: " + tokens);
+    for(QueryToken token : tokens)
+    {
+//      System.out.println("TOKEN: " + token);
+      this.getMap().put(token.getID(), token.getValue());
+    }
+    return new Query(this.getQuery(), this.getMap());
+  }
+  
+  @Override
+  public String toString()
+  {
+    return "[args: " + this.getArgs() + ", queryQueue: " + this.getQuery() + "]";
+  }
+  
+}
---
+++ src/crave/db/QueryToken.java
@@ -0,0 +1,45 @@
+
+public class QueryToken extends Object {
+  
+  private String id_, value_;
+  
+  public QueryToken(String id, String value)
+  {
+    super();
+    this.id_ = id;
+    this.value_ = value;
+  }
+  
+  public QueryToken(String id)
+  {
+    super();
+    this.id_ = id;
+    this.value_ = null;
+  }
+  
+  public String getID() { return this.id_; }
+  
+  public String getValue() { return this.value_; }
+  
+  public boolean add(String value)
+  {
+    if(this.getValue() != null) { return false; }
+    this.value_ = value;
+    return true;
+  }
+  
+  @Override
+  public int hashCode() 
+  {
+    if(this.getValue() == null) { return this.getID().hashCode(); }
+    return this.getID().hashCode() * this.getID().hashCode()
+      + this.getValue().hashCode();
+  }
+  
+  @Override
+  public String toString()
+  {
+    return "(" + this.getID() + ", " + this.getValue() + ")";
+  }
+  
+}
